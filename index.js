94af2142622d28e5220ea8084589dbd43f9f4e2e
Henri-Willekens <henriwillekens@gmail.com>
2021-11-24 15:29:16 +0100

    mqtt

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-
 const express = require("express");
 const app = express();
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://localhost');
 
+client.on('connect', function () {
+    client.subscribe('esp/test', function (err) {
+
+    })
+})
+
+client.on('message', function (topic, message){
+    //message is buffer
+    console.log(message.toString());
+    app.get('/', (req, res) => res.status(200).json(message.toString()));
+    chairData = message.toString();
+    var achair = JSON.parse(chairData);
+    console.log(achair.device);
+    console.log(achair.sensorType);
+    console.log(achair.values[0]);
+    console.log(achair.values[1]);
+    client.end();
+})
 
 app.get("/", function (req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3007, function () {
-    console.log("server is running on localhost 3007");
+app.listen(3090, function () {
+    console.log("server is running on localhost 3090");
 });
